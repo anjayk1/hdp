1a9df4b6838df2d8ea474b9393b3b521e65e10aa
anjayk1 <86257378+anjayk1@users.noreply.github.com>
2023-04-23 15:17:48 +0000

    commit

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def predictor():
     return render_template("predictor.html")
@@ -9,7 +10,9 @@ def predictor():
 @app.route("/hdp", methods=["POST"])
 def hdp():
     if request.method == "POST":
-        age = request.form.get(age)
-        print(request.form[])
-        print(request.form['gender'])
-    return 'Hello ' + request.form['age']
+        age = request.form.get[age]
+        chol = request.form[chol]
+        gender = request.form['gender']
+    return  age, chol, gender
+
+Age, Chol, Gender = hdp()
